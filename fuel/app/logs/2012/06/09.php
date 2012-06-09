cda03a36d631eb67b0d2eb956f355b4cbe04ccd6
Rati Wannapanop <rati.wannapanop@gmail.com>
2012-06-09 11:58:44 +0700

    un-track logs folder

--- fuel/app/logs/2012/06/09.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Error - 2012-06-09 01:01:06 --> Error - The requested view could not be found: dashboard/petro_app in D:\www\petro-test\fuel\core\classes\view.php on line 381
