67b82d917476a84f47459eef6f2745c7176a3afd
Rati Wannapanop <rati.wannapanop@gmail.com>
2012-06-20 22:52:55 +0700

    various update

---
+++ fuel/app/logs/2012/06/20.php
@@ -0,0 +1,31 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Error - 2012-06-20 11:52:09 --> 8 - Undefined index: id in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 240
+Error - 2012-06-20 11:52:09 --> 8 - Undefined index: username in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 241
+Error - 2012-06-20 11:52:09 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 243
+Error - 2012-06-20 11:52:09 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 244
+Error - 2012-06-20 15:49:17 --> 8 - Undefined index: id in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 240
+Error - 2012-06-20 15:49:17 --> 8 - Undefined index: username in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 241
+Error - 2012-06-20 15:49:17 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 243
+Error - 2012-06-20 15:49:17 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 244
+Error - 2012-06-20 16:15:12 --> Error - Class 'Petro\Form' not found in D:\www\petro-test\fuel\packages\petro\classes\controller\users.php on line 257
+Error - 2012-06-20 17:18:32 --> 8 - Array to string conversion in D:\www\petro-test\fuel\packages\petro\classes\petro\sidebar.php on line 58
+Error - 2012-06-20 17:18:58 --> Parsing Error - syntax error, unexpected ')' in D:\www\petro-test\fuel\packages\petro\classes\controller\users.php on line 98
+Error - 2012-06-20 21:43:21 --> 8 - Undefined index: id in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 240
+Error - 2012-06-20 21:43:21 --> 8 - Undefined index: username in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 241
+Error - 2012-06-20 21:43:21 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 243
+Error - 2012-06-20 21:43:21 --> 8 - Undefined index: metadata in D:\www\petro-test\fuel\packages\petro\classes\controller\app.php on line 244
+Error - 2012-06-20 22:07:58 --> Parsing Error - syntax error, unexpected '');' (T_CONSTANT_ENCAPSED_STRING) in D:\www\petro-test\fuel\packages\petro\classes\controller\users.php on line 92
+Error - 2012-06-20 22:15:58 --> 8 - Undefined variable: form in D:\www\petro-test\fuel\packages\petro\views\users\change_pwd.php on line 8
+Error - 2012-06-20 22:33:03 --> 8 - Undefined variable: form in D:\www\petro-test\fuel\packages\petro\views\users\change_pwd.php on line 8
+Error - 2012-06-20 22:40:56 --> 8 - Undefined offset: 0 in D:\www\petro-test\fuel\packages\petro\classes\petro\form.php on line 254
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in D:\www\petro-test\fuel\packages\petro\classes\petro\form.php on line 260
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in D:\www\petro-test\fuel\core\classes\arr.php on line 51
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in D:\www\petro-test\fuel\core\classes\arr.php on line 51
+Error - 2012-06-20 22:40:56 --> 8 - Array to string conversion in D:\www\petro-test\fuel\core\base.php on line 83
+Error - 2012-06-20 22:40:56 --> 8 - Array to string conversion in D:\www\petro-test\fuel\core\base.php on line 83
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in isset or empty in D:\www\petro-test\fuel\packages\petro\classes\petro\form.php on line 404
+Error - 2012-06-20 22:40:56 --> 8 - Array to string conversion in D:\www\petro-test\fuel\core\base.php on line 83
+Error - 2012-06-20 22:40:56 --> 8 - Undefined offset: 0 in D:\www\petro-test\fuel\packages\petro\classes\petro\form.php on line 254
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in D:\www\petro-test\fuel\packages\petro\classes\petro\form.php on line 260
+Error - 2012-06-20 22:40:56 --> 2 - Illegal offset type in D:\www\petro-test\fuel\core\classes\arr.php on line 51
